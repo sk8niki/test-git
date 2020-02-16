c7ba71c7c6c4a39aaabc53cc2d5542408ee64938
sk8niki <1134623854@qq.com>
2020-02-16 23:00:58 +0800

    测试提交到远程仓库

---
+++ HelloTest/src/com/nikihub/demo01/TestDev.java
@@ -0,0 +1,16 @@
+package com.nikihub.demo01;
+
+/**
+ * ProjectName:test-git
+ * Author: Rain
+ * Version: 1.0
+ * Date: Created in 2020/2/16 22:59
+ * Copyright (c) 2019, Rain All Rights Reserved.
+ * Description: //TODO
+ */
+public class TestDev {
+    public static void main(String[] args) {
+        System.out.println("TestDEV");
+        System.out.println("===========");
+    }
+}
