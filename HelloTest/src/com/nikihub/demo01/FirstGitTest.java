365aa6b0a7c3fb552d1308075972ea913a28d94a
sk8niki <1134623854@qq.com>
2020-02-16 21:27:59 +0800

    第一次提交HelloTest

---
+++ HelloTest/src/com/nikihub/demo01/FirstGitTest.java
@@ -0,0 +1,20 @@
+package com.nikihub.demo01;
+
+/**
+ * ProjectName:FirstGitTest
+ * Author: Rain
+ * Version: 1.0
+ * Date: Created in 2020/2/16 21:18
+ * Copyright (c) 2019, Rain All Rights Reserved.
+ * Description: //
+ */
+public class FirstGitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git test!!!");
+        System.out.println("Hello git test!!!");
+        System.out.println("Hello git test!!!");
+        System.out.println("Hello git test!!!");
+        System.out.println("Hello git test!!!");
+        System.out.println("Hello git test!!!");
+    }
+}
